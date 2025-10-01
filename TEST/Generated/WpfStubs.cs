4d89e5f847a6bc071c3457eb9749d53f6b3d5407
syyunfksmma <syyun@ksm.co.kr>
2025-10-01 16:35:37 +0900

    v22

--- TEST/Generated/WpfStubs.cs
+++ TEST/Generated/WpfStubs.cs
@@ -1,22 +1,42 @@
+// Build-time stub used when EnableWpfStubs=true to satisfy partial class definitions without XAML-generated code.
+// Do not enable in production WPF builds where .g.cs files exist, otherwise duplicate members will occur.
+using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media;
 
 namespace CAM_API.Feature.UI.Views
 {
     public partial class FeatureSelectionView
     {
+        private bool _contentLoaded;
+
         public void InitializeComponent()
         {
-            // Stub to satisfy build.
+            if (_contentLoaded)
+            {
+                return;
+            }
+
+            _contentLoaded = true;
+            var resourceLocater = new Uri("/CAM_API;component/Feature/UI/Views/FeatureSelectionView.xaml", UriKind.Relative);
+            Application.LoadComponent(this, resourceLocater);
         }
     }
 
     public partial class TagSelectedFacesView
     {
+        private bool _contentLoaded;
+
         public void InitializeComponent()
         {
-            // Stub to satisfy build.
+            if (_contentLoaded)
+            {
+                return;
+            }
+
+            _contentLoaded = true;
+            var resourceLocater = new Uri("/CAM_API;component/Feature/UI/Views/TagSelectedFacesView.xaml", UriKind.Relative);
+            Application.LoadComponent(this, resourceLocater);
         }
     }
 }
@@ -25,9 +45,18 @@ namespace CAM_API.KBM.UI.Views
 {
     public partial class DuplicateToolOpsWindow
     {
+        private bool _contentLoaded;
+
         public void InitializeComponent()
         {
-            // Stub to satisfy build.
+            if (_contentLoaded)
+            {
+                return;
+            }
+
+            _contentLoaded = true;
+            var resourceLocater = new Uri("/CAM_API;component/KBM/UI/Views/DuplicateToolOpsWindow.xaml", UriKind.Relative);
+            Application.LoadComponent(this, resourceLocater);
         }
     }
 }
@@ -36,8 +65,19 @@ namespace CAM_API.Setup.UI.Views
 {
     public partial class ProgramInfoSetupWindow
     {
+        private bool _contentLoaded;
+
         public void InitializeComponent()
         {
+            if (_contentLoaded)
+            {
+                return;
+            }
+
+            _contentLoaded = true;
+            var resourceLocater = new Uri("/CAM_API;component/Setup/UI/Views/ProgramInfoSetupWindow.xaml", UriKind.Relative);
+            Application.LoadComponent(this, resourceLocater);
+
             if (DataContext == null)
             {
                 DataContext = new CAM_API.Setup.UI.ViewModels.ProgramInfoSetupViewModel();
@@ -47,8 +87,19 @@ namespace CAM_API.Setup.UI.Views
 
     public partial class SetupWindow
     {
+        private bool _contentLoaded;
+
         public void InitializeComponent()
         {
+            if (_contentLoaded)
+            {
+                return;
+            }
+
+            _contentLoaded = true;
+            var resourceLocater = new Uri("/CAM_API;component/Setup/UI/Views/SetupWindow.xaml", UriKind.Relative);
+            Application.LoadComponent(this, resourceLocater);
+
             if (DataContext == null)
             {
                 DataContext = new CAM_API.Setup.UI.ViewModels.SetupWindowViewModel();
@@ -58,8 +109,19 @@ namespace CAM_API.Setup.UI.Views
 
     public partial class MachineTemplateEditWindow
     {
+        private bool _contentLoaded;
+
         public void InitializeComponent()
         {
+            if (_contentLoaded)
+            {
+                return;
+            }
+
+            _contentLoaded = true;
+            var resourceLocater = new Uri("/CAM_API;component/Setup/UI/Views/MachineTemplateEditWindow.xaml", UriKind.Relative);
+            Application.LoadComponent(this, resourceLocater);
+
             if (DataContext == null)
             {
                 DataContext = new CAM_API.Setup.UI.ViewModels.MachineTemplateEditViewModel();
@@ -72,6 +134,7 @@ namespace CAM_API.Volute_Create
 {
     public partial class VoluteWindow
     {
+        private bool _contentLoaded;
         private TextBox txtStartRadius;
         private TextBox txtEndRadius;
         private TextBox txtStartAngle;
@@ -86,23 +149,26 @@ namespace CAM_API.Volute_Create
 
         public void InitializeComponent()
         {
-            txtStartRadius = new TextBox();
-            txtEndRadius = new TextBox();
-            txtStartAngle = new TextBox();
-            txtEndAngle = new TextBox();
-            txtLeadOutRadius = new TextBox();
-            txtVoluteWidth = new TextBox();
-            txtVoluteDepth = new TextBox();
-            rbtnCW = new RadioButton();
-            rbtnCCW = new RadioButton();
-            ccw = new Image();
-            cw = new Image();
+            if (_contentLoaded)
+            {
+                return;
+            }
 
-            rbtnCCW.IsChecked = true;
-            rbtnCW.IsChecked = false;
+            _contentLoaded = true;
+            var resourceLocater = new Uri("/CAM_API;component/Volute/VoluteWindow.xaml", UriKind.Relative);
+            Application.LoadComponent(this, resourceLocater);
 
-            rbtnCCW.Checked += OnRadioButtonCheckedChanged;
-            rbtnCW.Checked += OnRadioButtonCheckedChanged;
+            txtStartRadius = (TextBox)FindName("txtStartRadius");
+            txtEndRadius = (TextBox)FindName("txtEndRadius");
+            txtStartAngle = (TextBox)FindName("txtStartAngle");
+            txtEndAngle = (TextBox)FindName("txtEndAngle");
+            txtLeadOutRadius = (TextBox)FindName("txtLeadOutRadius");
+            txtVoluteWidth = (TextBox)FindName("txtVoluteWidth");
+            txtVoluteDepth = (TextBox)FindName("txtVoluteDepth");
+            rbtnCW = (RadioButton)FindName("rbtnCW");
+            rbtnCCW = (RadioButton)FindName("rbtnCCW");
+            ccw = (Image)FindName("ccw");
+            cw = (Image)FindName("cw");
         }
     }
 }
